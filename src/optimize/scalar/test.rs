3adbd596a0cd24b3c14d3cc184c6a0ace0a42848
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2023-04-18 19:45:47 +0100

    Parabolic interpolation test

--- src/optimize/scalar/test.rs
+++ src/optimize/scalar/test.rs
@@ -2,8 +2,8 @@
 mod scalar_optimization {
 	use num_traits::Pow;
 	use crate::generals::Decimal;
-	use crate::optimize::scalar::bracket_optimizers::{bracket_gr_minimize};
-	use crate::optimize::scalar::bound_optimizers::{bound_gr_minimize as bound_gr};
+	use crate::optimize::scalar::bracket_optimizers::{bracket_gr_minimize, parabolic_interpolation};
+	use crate::optimize::scalar::bound_optimizers::bound_gr_minimize;
 
 	fn case_1(x: f64) -> f64{x.pow(2) + 6.0*x + 3.0	}
 
@@ -14,7 +14,7 @@ mod scalar_optimization {
 
 	#[test]
 	fn golden_ratio_case_1_bound_method() {
-		assert_eq!(bound_gr::<f64>(case_1, 4.0, -9.0, 1e-4, 2000).unwrap().round_dp(4), -3.0000)
+		assert_eq!(bound_gr_minimize::<f64>(case_1, 4.0, -9.0, 1e-4, 2000).unwrap().round_dp(4), -3.0000)
 	}
 
 	fn case_2(x: f64) -> f64 {x.pow(4) - 2.0 * x.pow(3) + 4.0}
@@ -26,6 +26,11 @@ mod scalar_optimization {
 
 	#[test]
 	fn golden_ratio_case_2_bound_method() {
-		assert_eq!(bound_gr::<f64>(case_2, 0.5, 2.0, 1e-4, 2000).unwrap().round_dp(4), 1.5000)
+		assert_eq!(bound_gr_minimize::<f64>(case_2, 0.5, 2.0, 1e-4, 2000).unwrap().round_dp(4), 1.5000)
+	}
+
+	#[test]
+	fn parabolic_interpolation_case2() {
+		assert_eq!(parabolic_interpolation(case_2, 0.5, 1.0, 2.0).unwrap().round_dp(6), 1.214286)
 	}
 }
